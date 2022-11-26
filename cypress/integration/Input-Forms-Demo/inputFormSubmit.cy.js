53eba4ca7c5e0620f3a0c93e2e1a4a3b7a66f4d2
nisha <knp>
2022-11-26 14:45:05 +0530

    changes

---
+++ cypress/integration/Input-Forms-Demo/inputFormSubmit.cy.js
@@ -0,0 +1,59 @@
+import InputFormSubmitPage from "./../../pages/inputFormSubmit";
+describe("Input form Demo", function () {
+  it("I am on the homepage of the Selenium Playground", function () {
+    cy.visit("/");
+  });
+  it("I click on the input form submit link in the input forms section", function () {
+    InputFormSubmitPage.getInputFormlink().click({ force: true });
+  });
+  it("I should navigate to the input form submit page", function () {
+    cy.url().should("include", "/input-form-demo");
+  });
+
+  it("I should see input form section", function () {
+    InputFormSubmitPage.getInputFormSection().should("be.visible");
+  });
+  it("I fill test data on the name input field", function () {
+    InputFormSubmitPage.getNameInputField().type("test data");
+  });
+  it("I fill email on the email input field", function () {
+    InputFormSubmitPage.getEmailInputField();
+  });
+  it("I fill password on the password input field", function () {
+    InputFormSubmitPage.getPasswordInputField();
+  });
+
+  it("I fill xyz.co on the company input field", function () {
+    InputFormSubmitPage.getCompanyName().type("XYZ.co");
+  });
+  it("I fill xyz.com on the website input field", function () {
+    InputFormSubmitPage.getWebsiteName().type("XYZhosting.com");
+  });
+  it("I fill test data on the country input field", function () {
+    InputFormSubmitPage.getCountryDropDown().select("India");
+  });
+  it("I fill delhi on the city input field", function () {
+    InputFormSubmitPage.getcityName().type("Delhi");
+  });
+  it("I fill dwarka morh on the address input field", function () {
+    InputFormSubmitPage.getAddressLocation().type("Dwarka Morh");
+  });
+  it("I fill west delhi on the address input field", function () {
+    InputFormSubmitPage.getSecondAddressLocation().type("West Delhi");
+  });
+  it("I fill delhi on the state input field", function () {
+    InputFormSubmitPage.getStateInputField().type("Delhi");
+  });
+  it("I fill 110059 data on the zip code input field", function () {
+    InputFormSubmitPage.getZipCode().type("110059");
+  });
+  it("I click on submit button", function () {
+    InputFormSubmitPage.getSubmitButton().click();
+  });
+  it("I should see the thanks message on the screen", function () {
+    InputFormSubmitPage.getOutputMessage().should(
+      "have.text",
+      "Thanks for contacting us, we will get back to you shortly."
+    );
+  });
+});
--- cypress/pages/inputFormSubmit.js
+++ cypress/pages/inputFormSubmit.js
@@ -1,4 +1,84 @@
-class InputFormSubmitPage{
+const inputFormlink = ".text-black.text-size-14:eq(4)";
+const inputFormSection = "div .wrapper .mt-50 .container";
+const emailInputField = "div #inputEmail4";
+const passwordInputField = "div #inputPassword4";
+const nameInputField = "div #name";
+const companyName = "div #company";
+const websiteName = "div #websitename";
+const cityName = "div #inputCity";
+const addressLocation = "div #inputAddress1";
+const secondAddressLocation = "div #inputAddress2";
+const stateInputField = "div #inputState";
+const zipCode = "div #inputZip";
+const countryDropDown = "div .pr-20 .w-full:eq('3')";
+const submitButton = "div .btn";
+const outputMessage = ".success-msg";
+class InputFormSubmitPage {
+  static getInputFormlink() {
+    return cy.get(inputFormlink);
+  }
+  static getInputFormSection() {
+    return cy.get(inputFormSection);
+  }
+  static getOutputMessage() {
+    return cy.get(outputMessage);
+  }
+  static getNameInputField() {
+    return cy.get(nameInputField);
+  }
+  static getCompanyName() {
+    return cy.get(companyName);
+  }
+  static getSubmitButton() {
+    return cy.get(submitButton);
+  }
+  static getCountryDropDown() {
+    return cy.get(countryDropDown);
+  }
+  static getZipCode() {
+    return cy.get(zipCode);
+  }
+  static getSecondAddressLocation() {
+    return cy.get(secondAddressLocation);
+  }
+  static getStateInputField() {
+    return cy.get(stateInputField);
+  }
+  static getcityName() {
+    return cy.get(cityName);
+  }
+  static getAddressLocation() {
+    return cy.get(addressLocation);
+  }
 
+  static getWebsiteName() {
+    return cy.get(websiteName);
+  }
+
+  static getEmailInputField() {
+    let email = this.getRandomEmail();
+    return cy.get(emailInputField).type(email);
+  }
+  static getRandomEmail() {
+    let str = " ";
+    let chars =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    for (let ii = 0; ii < 13; ii++) {
+      str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str + "@gmail.com";
+  }
+  static getPasswordInputField() {
+    let password = this.getRandomPassword();
+    return cy.get(passwordInputField).type(password);
+  }
+  static getRandomPassword() {
+    let num = " ";
+    let number = "0123456789";
+    for (let ii = 0; ii <= 10; ii++) {
+      num += number[Math.floor(Math.random * number.length)];
+    }
+    return num;
+  }
 }
 export default InputFormSubmitPage;
